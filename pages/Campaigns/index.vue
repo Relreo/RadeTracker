51d06099c2230af42192c5f7db340a3f9de36439
Ian Maynard <38901355+Relreo@users.noreply.github.com>
2022-04-15 16:10:52 -0700

    Creating and Deleting Campaigns
    Added basic logic for adding and deleting campaigns. Data being used is dummy data so the changes wont be persistent.

--- pages/Campaigns/index.vue
+++ pages/Campaigns/index.vue
@@ -43,8 +43,25 @@
                 </v-card-text>
                 <v-card-actions class="justify-center">
                     <v-btn depressed outlined :to="'campaigns/' + selectedCampaign.campaignID">Go to Campaign</v-btn>
+                    <v-btn depressed outlined color="red" @click="deleteConfirm = true">Delete Campaign</v-btn>
                 </v-card-actions>
             </v-card>
+            <v-dialog v-model="deleteConfirm" width="500">
+                <v-card flat outlined width="100%" class="mx-auto">
+                    <v-card-text>
+                        <div class="text-h3 mt-4 text-center">Are you sure?</div>
+                        <p class="text-h5 mt-8 text-center">
+                            This will also delete all cases associated with this campaign.
+                        </p>
+                    </v-card-text>
+                    <v-card-actions class="justify-center">
+                        <v-btn depressed outlined color="red" @click="deleteCampaign(selectedCampaign)"
+                            >Confirm Deletion</v-btn
+                        >
+                        <v-btn depressed outlined @click="deleteConfirm = false"> Cancel </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
         </v-dialog>
         <v-dialog v-model="createActive" width="500">
             <v-form ref="createForm" v-model="valid" class="mx-auto pa-8">
@@ -69,6 +86,8 @@
 
 <script>
 import PageTitle from '~/components/PageTitle.vue'
+
+const randomstring = require('randomstring')
 class Campaign {
     constructor(campaignName, campaignID) {
         this.campaignName = campaignName
@@ -86,6 +105,7 @@ export default {
     layout: 'data',
     data: () => ({
         campaignOpened: false,
+        deleteConfirm: false,
         createActive: false,
 
         selectedCampaign: {},
@@ -126,11 +146,35 @@ export default {
         openCreateMenu() {
             this.createActive = true
         },
+        generateCampaignID() {
+            return randomstring.generate({
+                length: 12,
+                charset: 'hex',
+            })
+        },
         createNewCampaign() {
             // GRAB FORM DATA AND ADD TO THE ARRAY AND EVENTUALLY THE DATABASE
             if (this.$refs.createForm.validate()) {
                 this.valid = true
+                let conflictFound = false
+                let newCampaignID = ''
+                do {
+                    newCampaignID = this.generateCampaignID()
+                    this.campaigns.forEach((element) => {
+                        if (element.campaignID === newCampaignID) conflictFound = true
+                    })
+                } while (conflictFound)
+
+                const newCampaign = new Campaign(this.createCampaignName, newCampaignID)
+                this.campaigns.push(newCampaign)
+                this.createActive = false
             }
+        },
+        deleteCampaign(campaign) {
+            const index = this.campaigns.indexOf(campaign)
+            if (index !== -1) this.campaigns.splice(index, 1)
+            if (this.campaignOpened) this.campaignOpened = false
+            if (this.deleteConfirm) this.deleteConfirm = false
         },
     },
 }
